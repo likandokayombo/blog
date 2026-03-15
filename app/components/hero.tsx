bc2782b0478d578048e8c3ddebcad4c431aa3210
Likando Kayomboe <likandokayombo2030@gmail.com>
2026-03-15 15:16:50 +0200

    chore: update hero description text

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -6,8 +6,7 @@ export default function Hero() {
         <h1 className="text-white sm:text-5xl font-bold mx-40">a process</h1>
         <p className="text-sm sm:text-lg text-white  text-center mt-5">
           Hello, i'm Likando this my blog where I explore posts and stories
-          about web development, UI design, and other interesting topics beyond
-          tech.
+          about web development, UI design, and other topics i find interesting
         </p>
       </div>
     </section>
